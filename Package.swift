360598ca6b03789a0cd3fb259d55430c0522a6f4
Jonathan Wight <jwight@mac.com>
2024-10-31 07:29:30 -0700

    Sketching out API.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,19 +8,11 @@ let package = Package(
         .macOS(.v15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Ultraviolence",
-            targets: ["Ultraviolence"]),
+        .library(name: "Ultraviolence", targets: ["Ultraviolence"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Ultraviolence"),
-        .testTarget(
-            name: "UltraviolenceTests",
-            dependencies: ["Ultraviolence"]
-        ),
+        .target(name: "Ultraviolence"),
+        .testTarget(name: "UltraviolenceTests", dependencies: ["Ultraviolence"]),
+        .executableTarget(name: "uvcli", dependencies: ["Ultraviolence"])
     ]
 )
