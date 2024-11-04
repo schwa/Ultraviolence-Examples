1c7e0de66d3f52fbb82c6cb263c730fda40ce5b2
Jonathan Wight <schwa@schwa.io>
2024-11-04 11:18:49 -0800

    swiftlint updates.

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 
 import PackageDescription
 
-let package = Package(
+public let package = Package(
     name: "Ultraviolence",
     platforms: [
         .macOS(.v15)
     ],
     products: [
         .library(name: "Ultraviolence", targets: ["Ultraviolence"]),
-        .library(name: "Examples", targets: ["Examples"]),
+        .library(name: "Examples", targets: ["Examples"])
     ],
     targets: [
         .target(name: "Ultraviolence", dependencies: ["BaseSupport"]),
         .target(name: "Examples", dependencies: ["Ultraviolence", "BaseSupport"], resources: [.copy("teapot.obj")]),
         .testTarget(name: "UltraviolenceTests", dependencies: ["Ultraviolence", "Examples", "BaseSupport"]),
         .executableTarget(name: "uvcli", dependencies: ["Ultraviolence", "Examples", "BaseSupport"]),
-        .target(name: "BaseSupport"),
+        .target(name: "BaseSupport")
     ],
-    swiftLanguageVersions: [.v6]
+    swiftLanguageModes: [.v6]
 )
