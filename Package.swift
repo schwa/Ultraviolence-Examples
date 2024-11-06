55d5f13c729413978eb5161c64c6c315819c287f
Jonathan Wight <jwight@mac.com>
2024-11-05 17:20:26 -0800

    Make once() w/macro.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version: 6.0
 
+import CompilerPluginSupport
 import PackageDescription
 
 public let package = Package(
@@ -12,12 +13,22 @@ public let package = Package(
         .library(name: "Ultraviolence", targets: ["Ultraviolence"]),
         .library(name: "Examples", targets: ["Examples"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest")
+    ],
     targets: [
         .target(name: "Ultraviolence", dependencies: ["UltraviolenceSupport"]),
         .target(name: "Examples", dependencies: ["Ultraviolence", "UltraviolenceSupport"], resources: [.copy("teapot.obj")]),
         .testTarget(name: "UltraviolenceTests", dependencies: ["Ultraviolence", "Examples", "UltraviolenceSupport"]),
         .executableTarget(name: "uvcli", dependencies: ["Ultraviolence", "Examples", "UltraviolenceSupport"]),
-        .target(name: "UltraviolenceSupport")
+        .target(name: "UltraviolenceSupport", dependencies: ["UltraviolenceMacros"]),
+        .macro(
+            name: "UltraviolenceMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ]
+        )
     ],
     swiftLanguageModes: [.v6]
 )
