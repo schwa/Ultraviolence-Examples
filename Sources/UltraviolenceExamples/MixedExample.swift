85137db6534c0d11d0aaa99614aedb112a4b3ec5
Jonathan Wight <jwight@mac.com>
2025-02-25 21:05:02 -0800

    .colorAttachment to .colorAttachment0

--- Sources/UltraviolenceExamples/MixedExample.swift
+++ Sources/UltraviolenceExamples/MixedExample.swift
@@ -31,7 +31,7 @@ public struct MixedExample: Element {
             try RenderPass {
                 try TeapotDemo(modelMatrix: modelMatrix, color: color, lightDirection: lightDirection)
                     // TODO: Next two lines are only needed for the offscreen examples?
-                    .colorAttachment(colorTexture, index: 0)
+                    .colorAttachment0(colorTexture, index: 0)
                     .depthAttachment(depthTexture)
             }
             .renderPassModifier { renderPassDescriptor in
