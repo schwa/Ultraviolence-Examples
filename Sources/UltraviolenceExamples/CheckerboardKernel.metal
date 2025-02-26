20e13e7ad143de0a283b4161eee8b64e144611ce
Jonathan Wight <jwight@mac.com>
2025-02-25 21:05:02 -0800

    Break checkerboard kernel into two for now...

--- Sources/UltraviolenceExamples/CheckerboardKernel.metal
+++ Sources/UltraviolenceExamples/CheckerboardKernel.metal
@@ -5,14 +5,26 @@ using namespace metal;
 
 uint2 gid [[thread_position_in_grid]];
 
-kernel void CheckerboardKernel(
+kernel void CheckerboardKernel_float4(
     texture2d<float, access::read_write> outputTexture [[texture(0)]],
     constant float2 &checkerSize [[buffer(0)]],
     constant float4 &backgroundColor[[buffer(1)]],
     constant float4 &foregroundColor[[buffer(2)]]
 ) {
-    float2 checkerCoord = floor(float2(gid) / checkerSize);
-    float checkerValue = fmod(checkerCoord.x + checkerCoord.y, 2.0);
-    float4 color = (checkerValue == 0.0) ? backgroundColor : foregroundColor;
+    const float2 checkerCoord = floor(float2(gid) / checkerSize);
+    const float checkerValue = fmod(checkerCoord.x + checkerCoord.y, 2.0);
+    const auto color = (checkerValue == 0.0) ? backgroundColor : foregroundColor;
+    outputTexture.write(color, gid);
+}
+
+kernel void CheckerboardKernel_ushort(
+    texture2d<ushort, access::read_write> outputTexture [[texture(0)]],
+    constant float2 &checkerSize [[buffer(0)]],
+    constant ushort &backgroundColor[[buffer(1)]],
+    constant ushort &foregroundColor[[buffer(2)]]
+) {
+    const float2 checkerCoord = floor(float2(gid) / checkerSize);
+    const float checkerValue = fmod(checkerCoord.x + checkerCoord.y, 2.0);
+    const auto color = (checkerValue == 0.0) ? backgroundColor : foregroundColor;
     outputTexture.write(color, gid);
 }
--- Sources/UltraviolenceExamples/CheckerboardKernel.swift
+++ Sources/UltraviolenceExamples/CheckerboardKernel.swift
@@ -10,7 +10,34 @@ public struct CheckerboardKernel: Element {
     private var foregroundColor: SIMD4<Float>
 
     init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, backgroundColor: SIMD4<Float>, foregroundColor: SIMD4<Float>) throws {
-        kernel = try ShaderLibrary(bundle: .module).CheckerboardKernel
+        kernel = try ShaderLibrary(bundle: .module).CheckerboardKernel_float4
+        self.outputTexture = outputTexture
+        self.checkerSize = checkerSize
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+    }
+
+    public var body: some Element {
+        ComputePipeline(computeKernel: kernel) {
+            // TODO: Compute threads per threadgroup
+            ComputeDispatch(threads: .init(width: outputTexture.width, height: outputTexture.height, depth: 1), threadsPerThreadgroup: .init(width: 32, height: 32, depth: 1))
+                .parameter("outputTexture", texture: outputTexture)
+                .parameter("checkerSize", value: checkerSize)
+                .parameter("backgroundColor", value: backgroundColor)
+                .parameter("foregroundColor", value: foregroundColor)
+        }
+    }
+}
+
+public struct CheckerboardKernel_ushort: Element {
+    private var kernel: ComputeKernel
+    private var outputTexture: MTLTexture
+    private var checkerSize: SIMD2<Float>
+    private var backgroundColor: UInt16
+    private var foregroundColor: UInt16
+
+    init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, backgroundColor: UInt16, foregroundColor: UInt16) throws {
+        kernel = try ShaderLibrary(bundle: .module).CheckerboardKernel_ushort
         self.outputTexture = outputTexture
         self.checkerSize = checkerSize
         self.backgroundColor = backgroundColor
