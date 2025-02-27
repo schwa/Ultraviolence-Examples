a7001c0e10a09e21bea626664c10cd874e8dee42
Jonathan Wight <jwight@mac.com>
2025-02-27 15:07:11 -0800

    Checkerboards only write foreground color.

--- Sources/UltraviolenceExamples/CheckerboardKernel.metal
+++ Sources/UltraviolenceExamples/CheckerboardKernel.metal
@@ -8,23 +8,23 @@ uint2 gid [[thread_position_in_grid]];
 kernel void CheckerboardKernel_float4(
     texture2d<float, access::read_write> outputTexture [[texture(0)]],
     constant float2 &checkerSize [[buffer(0)]],
-    constant float4 &backgroundColor[[buffer(1)]],
     constant float4 &foregroundColor[[buffer(2)]]
 ) {
     const float2 checkerCoord = floor(float2(gid) / checkerSize);
     const float checkerValue = fmod(checkerCoord.x + checkerCoord.y, 2.0);
-    const auto color = (checkerValue == 0.0) ? backgroundColor : foregroundColor;
-    outputTexture.write(color, gid);
+    if (checkerValue != 0.0) {
+        outputTexture.write(foregroundColor, gid);
+    }
 }
 
 kernel void CheckerboardKernel_ushort(
     texture2d<ushort, access::read_write> outputTexture [[texture(0)]],
     constant float2 &checkerSize [[buffer(0)]],
-    constant ushort &backgroundColor[[buffer(1)]],
     constant ushort &foregroundColor[[buffer(2)]]
 ) {
     const float2 checkerCoord = floor(float2(gid) / checkerSize);
     const float checkerValue = fmod(checkerCoord.x + checkerCoord.y, 2.0);
-    const auto color = (checkerValue == 0.0) ? backgroundColor : foregroundColor;
-    outputTexture.write(color, gid);
+    if (checkerValue != 0.0) {
+        outputTexture.write(foregroundColor, gid);
+    }
 }
--- Sources/UltraviolenceExamples/CheckerboardKernel.swift
+++ Sources/UltraviolenceExamples/CheckerboardKernel.swift
@@ -7,14 +7,12 @@ public struct CheckerboardKernel: Element {
     private var kernel: ComputeKernel
     private var outputTexture: MTLTexture
     private var checkerSize: SIMD2<Float>
-    private var backgroundColor: SIMD4<Float>
     private var foregroundColor: SIMD4<Float>
 
-    public init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, backgroundColor: SIMD4<Float>, foregroundColor: SIMD4<Float>) throws {
+    public init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, foregroundColor: SIMD4<Float>) throws {
         kernel = try ShaderLibrary(bundle: .module).CheckerboardKernel_float4
         self.outputTexture = outputTexture
         self.checkerSize = checkerSize
-        self.backgroundColor = backgroundColor
         self.foregroundColor = foregroundColor
     }
 
@@ -24,7 +22,6 @@ public struct CheckerboardKernel: Element {
             ComputeDispatch(threads: .init(width: outputTexture.width, height: outputTexture.height, depth: 1), threadsPerThreadgroup: .init(width: 32, height: 32, depth: 1))
                 .parameter("outputTexture", texture: outputTexture)
                 .parameter("checkerSize", value: checkerSize)
-                .parameter("backgroundColor", value: backgroundColor)
                 .parameter("foregroundColor", value: foregroundColor)
         }
     }
@@ -35,14 +32,12 @@ public struct CheckerboardKernel_ushort: Element {
     private var kernel: ComputeKernel
     private var outputTexture: MTLTexture
     private var checkerSize: SIMD2<Float>
-    private var backgroundColor: UInt16
     private var foregroundColor: UInt16
 
-    public init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, backgroundColor: UInt16, foregroundColor: UInt16) throws {
+    public init(outputTexture: MTLTexture, checkerSize: SIMD2<Float>, foregroundColor: UInt16) throws {
         kernel = try ShaderLibrary(bundle: .module).CheckerboardKernel_ushort
         self.outputTexture = outputTexture
         self.checkerSize = checkerSize
-        self.backgroundColor = backgroundColor
         self.foregroundColor = foregroundColor
     }
 
@@ -52,7 +47,6 @@ public struct CheckerboardKernel_ushort: Element {
             ComputeDispatch(threads: .init(width: outputTexture.width, height: outputTexture.height, depth: 1), threadsPerThreadgroup: .init(width: 32, height: 32, depth: 1))
                 .parameter("outputTexture", texture: outputTexture)
                 .parameter("checkerSize", value: checkerSize)
-                .parameter("backgroundColor", value: backgroundColor)
                 .parameter("foregroundColor", value: foregroundColor)
         }
     }
@@ -66,7 +60,7 @@ extension CheckerboardKernel: Example {
         textureDescriptor.usage = [.shaderWrite, .shaderRead]
         let texture = try device.makeTexture(descriptor: textureDescriptor, value: UInt32(0xFF000000))
         let pass = try ComputePass {
-            try CheckerboardKernel(outputTexture: texture, checkerSize: [32, 32], backgroundColor: [0, 0, 0, 1], foregroundColor: [1, 1, 1, 1])
+            try CheckerboardKernel(outputTexture: texture, checkerSize: [32, 32], foregroundColor: [1, 1, 1, 1])
         }
         try pass.run()
         return .texture(texture)
