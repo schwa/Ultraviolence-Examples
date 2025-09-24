f2a1fedffc09658aaace5005c99e6484dbcd6576
Jonathan Wight <jwight@mac.com>
2025-09-23 20:41:16 -0700

    Break out from main repo

---
+++ Packages/UltraviolenceExamples/Package.swift
@@ -0,0 +1,74 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UltraviolenceExamples",
+    platforms: [
+        .iOS("18.6"),
+        .macOS("15.6"),
+        .visionOS("2.6")
+    ],
+    products: [
+        .library(name: "UltraviolenceExamples", targets: ["UltraviolenceExamples"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/schwa/Ultraviolence", branch: "main"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(url: "https://github.com/schwa/MetalCompilerPlugin", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-collections", from: "1.2.0"),
+        .package(url: "https://github.com/schwa/GeometryLite3D", branch: "main"),
+        .package(url: "https://github.com/schwa/SwiftGLTF", branch: "main"),
+        .package(url: "https://github.com/schwa/Panels", branch: "main"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UltraviolenceExamples",
+            dependencies: [
+                "UltraviolenceExampleShaders",
+                "MikkTSpace",
+                .product(name: "Ultraviolence", package: "Ultraviolence"),
+                .product(name: "UltraviolenceUI", package: "Ultraviolence"),
+                .product(name: "Collections", package: "swift-collections"),
+                .product(name: "GeometryLite3D", package: "GeometryLite3D"),
+                .product(name: "SwiftGLTF", package: "SwiftGLTF"),
+                .product(name: "Panels", package: "Panels"),
+                .product(name: "ZIPFoundation", package: "ZIPFoundation"),
+            ],
+            resources: [
+                .copy("Resources/AppleEventVideo.mp4"),
+                .copy("Resources/AppleLogoMask.png"),
+                .copy("Resources/DJSI3956.JPG"),
+                .copy("Resources/DSC_2595.JPG"),
+                .copy("Resources/HD-Testcard-original.jpg"),
+                .copy("Resources/IndoorEnvironmentHDRI013_1K-HDR.exr"),
+                .copy("Resources/Samples"),
+                .copy("Resources/teapot.obj"),
+                .copy("Resources/4.2.03.heic"),
+            ],
+            swiftSettings: [
+//                .defaultIsolation(nil)
+            ]
+        ),
+        .testTarget(
+            name: "UltraviolenceExamplesTests",
+            dependencies: ["UltraviolenceExamples"]
+        ),
+        .target(
+            name: "UltraviolenceExampleShaders",
+            publicHeadersPath: ".",
+            plugins: [
+                .plugin(name: "MetalCompilerPlugin", package: "MetalCompilerPlugin")
+            ]
+        ),
+        .target(
+            name: "MikkTSpace",
+            publicHeadersPath: ".",
+        )
+    ],
+    swiftLanguageModes: [.v6]
+)
