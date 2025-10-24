99dd661c46489507bf9719389626d51cf9e46bc2
Jonathan Wight <jwight@mac.com>
2025-10-24 09:52:32 -0700

    Add the (broken) rotation widget to PBR view for demo purposes.

--- Packages/UltraviolenceExamples/Sources/UltraviolenceExamples/LightingModels/PBR/PBRDemoView.swift
+++ Packages/UltraviolenceExamples/Sources/UltraviolenceExamples/LightingModels/PBR/PBRDemoView.swift
@@ -86,6 +86,17 @@ public struct PBRDemoView: View {
                 }
             }
         }
+        .overlay(alignment: .topTrailing) {
+            // TODO: This technically wont work with the turntable modifier because the turn table has a radius from the rotation point
+                VStack {
+                    RotationWidget(rotation: $cameraMatrix.rotation)
+                        .frame(width: 120, height: 120)
+                    Text("(broken)").foregroundStyle(.orange)
+                }
+                .padding()
+                .background(Color.black, in: RoundedRectangle(cornerRadius: 8))
+                .padding()
+        }
         .onChange(of: selectedMaterial, initial: false) {
             if selectedMaterial == .custom {
                 customMaterial = PBRMaterialNew()
@@ -101,5 +112,53 @@ public struct PBRDemoView: View {
 
     private var currentMaterial: PBRMaterialNew {
         selectedMaterial == .custom ? customMaterial : selectedMaterial.material
+    }
+}
+
+extension float4x4 {
+    var rotation: simd_quatf {
+        get {
+            simd_quatf(self)
+        }
+        set {
+            guard var components = decompose else {
+                fatalError("Could not decompose transform")
+            }
+            components.rotation = newValue
+            self = .init(components: components)
+        }
+    }
+
+    init(components: TransformComponents) {
+        // Build transformation matrix from components
+        // Order: Scale -> Skew -> Rotation -> Translation -> Perspective
+
+        // Start with scale matrix
+        var matrix = float4x4(diagonal: [components.scale.x, components.scale.y, components.scale.z, 1])
+
+        // Apply skew
+        let skewMatrix = float4x4(
+            [1, components.skew.xy, components.skew.xz, 0],
+            [0, 1, components.skew.yz, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        )
+        matrix = skewMatrix * matrix
+
+        // Apply rotation
+        let rotationMatrix = float4x4(components.rotation)
+        matrix = rotationMatrix * matrix
+
+        // Apply translation
+        matrix.columns.3 = [components.translate.x, components.translate.y, components.translate.z, 1]
+
+        // Apply perspective (if needed)
+        // Perspective is typically applied to the last row
+        matrix[0][3] = components.perspective.x
+        matrix[1][3] = components.perspective.y
+        matrix[2][3] = components.perspective.z
+        matrix[3][3] = components.perspective.w
+
+        self = matrix
     }
 }
